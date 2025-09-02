e95b5a6f45c53432ebe2dd5eef2e05403997a8d9
박상혁(SANGHYEOKBAK) <suran5281@gmail.com>
2025-09-02 14:40:24 +0900

    fix: UnsafeRestClientFactory 수정(불필요한 ipmort문 제거)

--- src/main/java/com/shbak/RestClientClass/utils/UnsafeRestClientFactory.java
+++ src/main/java/com/shbak/RestClientClass/utils/UnsafeRestClientFactory.java
@@ -5,8 +5,6 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
