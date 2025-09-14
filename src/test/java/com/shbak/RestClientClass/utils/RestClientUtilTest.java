ca8b81d6e9848f1d7688147e33d208b3e6266a31
psh94 <suran5281@gmail.com>
2025-09-14 22:15:58 +0900

    test: post, patch, delete 정상 호출 테스트 코드 작성

--- src/test/java/com/shbak/RestClientClass/utils/RestClientUtilTest.java
+++ src/test/java/com/shbak/RestClientClass/utils/RestClientUtilTest.java
@@ -21,13 +21,14 @@ class RestClientUtilTest {
 
     @Test
     @DisplayName("GET 성공 -> ignoreSsl=true (unsafe 사용)")
-    void test1() {
+    void get_success_when_ignoreSsl_true() {
         ReflectionTestUtils.setField(util, "ignoreSsl", true);
-        String url = "https://www.mxfleet.net/index.html"; // 정상 인증된 HTTPS
+        String url = "https://www.mxfleet.net/index.html";
+//        String url = "https://self-signed.badssl.com/"; // SSL 테스트용 사이트
         ResponseEntity<String> res = util.getEntity(url);
 
         // 출력 (요청 결과와 상태)
-        System.out.println("[GET REQUEST] : " + url);
+        System.out.println("[URL] : " + url);
         System.out.println("[STATUS] : " + res.getStatusCode());
         System.out.println("[BODY]   : " + res.getBody());
 
@@ -36,11 +37,63 @@ class RestClientUtilTest {
 
     @Test
     @DisplayName("GET 요청 실패 테스트 -> ignoreSsl: false")
-    void test2() {
+    void get_fail_when_ignoreSsl_false() {
         ReflectionTestUtils.setField(util, "ignoreSsl", false);
-        String url = "https://www.mxfleet.net/index.html"; // 정상 인증된 HTTPS
+        String url = "https://www.mxfleet.net/index.html";
+//        String url = "https://self-signed.badssl.com/"; // SSL 테스트용 사이트
 
-        RuntimeException error = assertThrows(ResourceAccessException.class, () -> util.get(url));
+        RuntimeException error = assertThrows(RuntimeException.class, () -> util.get(url));
         System.out.println("[ERROR] : " + error.getMessage());
     }
+
+    /**
+     * POST, PATCH, DELETE 테스트
+     * - SSL 무시 여부를 확인할 수 있는 공개 테스트 사이트가 마땅히 없다고 판단함
+     * - 따라서 ignoreSsl=true로 고정하여 정상 호출 여부만 테스트 함
+     */
+    @Test
+    @DisplayName("POST 성공 테스트")
+    void post_success() {
+        ReflectionTestUtils.setField(util, "ignoreSsl", true);
+        String url = "https://httpbin.org/post"; // SSL 테스트용 사이트
+        ResponseEntity<String> res = util.post(url, "post request test body: post post ...");
+
+        // 출력 (요청 결과와 상태)
+        System.out.println("[URL] : " + url);
+        System.out.println("[STATUS] : " + res.getStatusCode());
+        System.out.println("[BODY]   : " + res.getBody());
+
+        assertTrue(res.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("PATCH 성공 테스트")
+    void patch_success() {
+        ReflectionTestUtils.setField(util, "ignoreSsl", true);
+        String url = "https://httpbin.org/patch"; // SSL 테스트용 사이트
+        ResponseEntity<String> res = util.patch(url, "post request test body: post post ...");
+
+        // 출력 (요청 결과와 상태)
+        System.out.println("[URL] : " + url);
+        System.out.println("[STATUS] : " + res.getStatusCode());
+        System.out.println("[BODY]   : " + res.getBody());
+
+        assertTrue(res.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("DELETE 성공 테스트")
+    void delete_success() {
+        ReflectionTestUtils.setField(util, "ignoreSsl", true);
+        String url = "https://httpbin.org/delete"; // SSL 테스트용 사이트
+        ResponseEntity<String> res = util.delete(url);
+
+        // 출력 (요청 결과와 상태)
+        System.out.println("[URL] : " + url);
+        System.out.println("[STATUS] : " + res.getStatusCode());
+        System.out.println("[BODY]   : " + res.getBody());
+
+        assertTrue(res.getStatusCode().is2xxSuccessful());
+    }
+
 }
